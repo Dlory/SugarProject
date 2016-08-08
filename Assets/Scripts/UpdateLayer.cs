b52ae8adcae61035758ca2ffce4e319d6c8999f0
sugar <602285525@qq.com>
2016-08-08 18:03:37 +0800

    20160808版本（带有实时层级效果）

--- Assets/Scripts/UpdateLayer.cs
+++ Assets/Scripts/UpdateLayer.cs
@@ -8,31 +8,21 @@ public class UpdateLayer : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag ("Player");
-
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		playerY = player.transform.localPosition.y;
 		myY = transform.localPosition.y;
-		Debug.Log ("PlayerY:" + playerY + ",myY:" + myY);
 		if (myY < playerY) {
 			int myOrder = transform.GetComponent<SpriteRenderer> ().sortingOrder;
-			myOrder = 1;
+			myOrder = 2;
 			transform.GetComponent<SpriteRenderer> ().sortingOrder = myOrder;
-			int playerOrder = player.transform.GetComponent<SpriteRenderer> ().sortingOrder;
-			playerOrder = 0;
-			player.transform.GetComponent<SpriteRenderer> ().sortingOrder = playerOrder;
 		} 
 		else {
 			int myOrder = transform.GetComponent<SpriteRenderer> ().sortingOrder;
 			myOrder = 0;
 			transform.GetComponent<SpriteRenderer> ().sortingOrder = myOrder;
-			int playerOrder = player.transform.GetComponent<SpriteRenderer> ().sortingOrder;
-			playerOrder = 1;
-			player.transform.GetComponent<SpriteRenderer> ().sortingOrder = playerOrder;
-		
 		}
 	}
 }
