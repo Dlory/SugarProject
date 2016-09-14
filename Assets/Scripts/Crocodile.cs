960f30f6a394aba5b13f5883d1eacb9fd5f01765
sugar <602285525@qq.com>
2016-09-14 18:54:13 +0800

    鳄鱼基本动画

--- Assets/Scripts/Crocodile.cs
+++ Assets/Scripts/Crocodile.cs
@@ -3,37 +3,61 @@ using System.Collections;
 
 public class Crocodile : MonoBehaviour {
 	public Animator animator;
-	bool wait = false;
+	public Animator FragmentAnimator;
+	public GameObject Fragment;
 	private float percent;
+	int type = 0;
 	// Use this for initialization
 	void Start () {
-		animator.PlayInFixedTime("CrocodileIdle", 0,0);
+		Fragment.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (wait) {
-			Debug.Log ("teat");
-			animator.PlayInFixedTime ("CrocodileIdle", 0, percent);
-		}
+		
 	}
-	public void Wait(){
-		percent = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
-		wait = true;
-		Invoke ("ContinuePlay",Random.Range(2,3));
+	public void Type(){
+		type = Random.Range (1, 3);
+		Invoke ("ChangeState", Random.Range (0f, 1.5f));
+	}
 
+	private void ChangeState(){
+		animator.SetTrigger(GetTriggerName());
 	}
-	private void ContinuePlay(){
-		wait = false;
-		animator.PlayInFixedTime("CrocodileIdle", 0, percent);
-	}
+
 
 	public void StopAndRepeat(){
+		type = 0;
 		gameObject.SetActive (false);
-		Invoke ("Again",Random.Range(0.5f,1.5f));
+		Invoke ("Again",Random.Range(0.5f,2f));
 	}
+
 	private void Again(){
 		gameObject.SetActive (true);
-		animator.PlayInFixedTime("CrocodileIdle", 0,0);
+
 	}
+	private string GetTriggerName()
+	{
+		switch (type) {
+		case 1:
+			return "Wink";
+		case 2:
+			return "Disappear";
+		default:
+			return "";
+		}
+	}
+
+	public void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {     //鳄鱼被干扰产生干扰波
+			animator.SetTrigger("Interfere");
+//			animator.SetTrigger("Crush");        //鳄鱼被砸（未完成）
+//			if (Fragment.active) {
+//				Fragment.SetActive (true);
+//				FragmentAnimator.SetTrigger ("Spit");
+//			}
+		}
+	}
+
+
 }
